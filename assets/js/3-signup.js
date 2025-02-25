7f022239f2053589ba0cfbe295b9d1fc9fe73124
BaharAlmasiMoghaddam <baharalmasiimoghaddam@gmail.com>
2025-02-25 23:17:40 +0330

    add unfinished api section - 1st try!

--- assets/js/3-signup.js
+++ assets/js/3-signup.js
@@ -1,9 +1,12 @@
+// **** still need to work on this js *****
+
 const signupForm = document.getElementById("signup-form");
 const loginForm = document.getElementById("login-form");
 const signupTab = document.getElementById("signup-tab");
 const loginTab = document.getElementById("login-tab");
 const toggleSwitch = document.querySelector("#dark-mode-toggle");
 
+//
 //
 // sign/log switch:
 function showForm(formType) {
@@ -20,6 +23,8 @@ function showForm(formType) {
   }
 }
 
+//
+//
 // dark mode:
 document.addEventListener("DOMContentLoaded", () => {
   toggleSwitch.addEventListener("change", () => {
@@ -30,4 +35,42 @@ document.addEventListener("DOMContentLoaded", () => {
       localStorage.setItem("darkMode", "disabled");
     }
   });
+});
+
+//
+//
+// using api section:
+
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const email = loginForm.querySelector("input[type='email']").value;
+  const password = loginForm.querySelector("input[type='password']").value;
+
+  // ====== need to find a good one api ========
+  //this below one is bad request + doesn't work
+  // https://apitester.ir/api/Users/authenticate
+  try {
+    const response = await fetch("?", {
+      method: "POST", //check this one too
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: email,
+        password: password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      alert("Login successful!");
+    } else {
+      alert(`Login failed: ${data.message}`);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    alert("An error occurred during login.");
+  }
 });
